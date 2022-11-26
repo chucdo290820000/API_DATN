63b457fae9f90f3ee21448c1b9f5499c33213c19
chucdo29082000 <dochuc086@gmail.com>
2022-11-26 09:13:05 +0700

    apidatn

---
+++ blog/routes/api.php
@@ -0,0 +1,18 @@
+<?php
+use Illuminate\Routing\Route;
+
+Route::group([
+    'prefix' => 'auth'
+], function () {
+    Route::post('login', 'AuthController@login');
+    Route::post('signup', 'AuthController@signup');
+  
+    Route::group([
+      'middleware' => 'auth:api'
+    ], function() {
+        Route::get('logout', 'AuthController@logout');
+        Route::get('user', 'AuthController@user');
+    });
+}
+);
+?>
